47a01ec1bb1021d5d5551572ab8fc00d7aa57e6e
Aurelie <3kdapp25.vaudau@folkuniversitetet.nu>
2026-02-04 20:17:19 +0100

    Added initial logic for loginStates

---
+++ app/src/main/java/com/example/warcardgamecompose/LoginUiState.kt
@@ -0,0 +1,7 @@
+package com.example.warcardgamecompose
+
+data class LoginUiState(
+    val email: String = "",
+    val password: String = ""
+
+)
---
+++ app/src/main/java/com/example/warcardgamecompose/LoginViewModel.kt
@@ -0,0 +1,16 @@
+package com.example.warcardgamecompose
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class LoginViewModel: ViewModel() {
+    private val _uiState = MutableStateFlow(LoginUiState())
+    val uiState: StateFlow<LoginUiState> = _uiState.asStateFlow()
+
+    fun onEmailChange(email: String) {
+        _uiState.update {it.copy(email = email)}
+    }
+}
